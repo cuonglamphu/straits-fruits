f418c4875efd99707e4170b13fb560fa60f0f9de
itsphucuong <lpcuong8888@gmail.com>
2024-06-21 08:59:20 +0700

    implement middleware

--- routes/web.php
+++ routes/web.php
@@ -5,43 +5,48 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\manageInvoicesController;
 use App\Http\Controllers\pdfController;
 
-Route::get('/', function () {
-    return view('invoice');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-
-Route::get('/categoryregis', function () {
-    return view('category');
-})->name('categoryregis');
-Route::get('/fruitregis', function () {
-    return view('fruit');
-})->name('fruitregis');
-Route::get('/newinvoice', function () {
-    return view('invoice');
-})->name('newinvoice');
-Route::get('/manageinvoices', function () {
-    return view('manageInvoices');
-})->name('manageinvoices');
-
-//pdf with id route
-Route::get('/pdf/{id}', [pdfController::class, 'index'])->name('pdf');
-//manage invoices route
-Route::get('/manageinvoices', [manageInvoicesController::class, 'index'])->name('manageinvoices');
-//get table data route
-Route::get('/getTableData', [manageInvoicesController::class, 'getTableData'])->name('getTableData');
-
-//edit invoice
-Route::get('/invoices/{id}/edit', [manageInvoicesController::class, 'edit'])->name('invoices.edit');
-
+// Routes with 'auth' middleware
 Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('invoice');
+    })->name('dashboard');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::get('/categoryregis', function () {
+        return view('category');
+    })->name('categoryregis');
+
+    Route::get('/fruitregis', function () {
+        return view('fruit');
+    })->name('fruitregis');
+
+    Route::get('/newinvoice', function () {
+        return view('invoice');
+    })->name('newinvoice');
+
+    Route::get('/manageinvoices', function () {
+        return view('manageInvoices');
+    })->name('manageinvoices');
+
+    // pdf with id route
+    Route::get('/pdf/{id}', [pdfController::class, 'index'])->name('pdf');
+
+    // manage invoices route
+    Route::get('/manageinvoices', [manageInvoicesController::class, 'index'])->name('manageinvoices');
+
+    // get table data route
+    Route::get('/getTableData', [manageInvoicesController::class, 'getTableData'])->name('getTableData');
+
+    // edit invoice
+    Route::get('/invoices/{id}/edit', [manageInvoicesController::class, 'edit'])->name('invoices.edit');
+
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';
